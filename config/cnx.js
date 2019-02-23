d317fc4584b48b29cc3d83cd58aa07dbd6d0f3a7
Houssnou <hosni.moubarak@gmail.com>
2019-02-23 15:55:05 -0500

    updating cnx.js

--- config/cnx.js
+++ config/cnx.js
@@ -3,9 +3,14 @@ var mongoose = require("mongoose");
 let cnx;
 
 // Connect to the Mongo DB
-cnx = mongoose.connect("process.env.MONGODB_URI"||"mongodb://localhost/hardware_reviews_db", {
+if (process.env.MONGODB_URI){
+  cnx = mongoose.connect("process.env.MONGODB_URI", {
   useNewUrlParser: true
 });
-
+}else{
+cnx = mongoose.connect("mongodb://localhost/hardware_reviews_db", {
+  useNewUrlParser: true
+});
+}
 // Export the connection so it's available in other parts of the app
 module.exports = cnx;
